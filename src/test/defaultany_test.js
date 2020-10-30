939967be5524e97563d20c2c384d6deb86cbe52e
Niladri Saha <niladri.saha@tuni.fi>
2020-10-31 01:30:22 +0530

    Test

---
+++ src/test/defaultany_test.js
@@ -0,0 +1,26 @@
+import chai from 'chai';
+const expect = chai.expect;
+const assert = chai.assert;
+//const {expect,assert} = require('chai');
+import defaultToAny from '../defaultToAny.js';
+//const fs = require("../add");
+describe("Integration Testing setting value with multiple default values",()=>{
+    it("Default for normal cases", function(){
+    const result = defaultToAny(2,5,6);
+    expect(result).to.equal(2);
+    });
+    it("Default for falsy case 1",function(){
+    const result1 = defaultToAny(undefined,5,10);
+    expect(result1).to.equal(5);
+    });
+    it("Default for falsy case 2",function(){
+        const result2 = defaultToAny(null,undefined,10);
+        expect(result2).to.equal(10);
+        });
+        it("Default for falsy case 3",function(){
+            const result2 = defaultToAny(null,undefined,NaN);
+            expect(result2).to.deep.equal(NaN);
+            });
+    
+
+    });
---
+++ src/test/defaultto_test.js
@@ -0,0 +1,22 @@
+import chai from 'chai';
+const expect = chai.expect;
+const assert = chai.assert;
+//const {expect,assert} = require('chai');
+import defaultTo from '../defaultTo.js';
+//const fs = require("../add");
+describe("Setting Default Values",()=>{
+    it("Default for normal cases", function(){
+    const result = defaultTo(2,5);
+    expect(result).to.equal(2);
+    });
+    it("Default for falsy case 1",function(){
+    const result1 = defaultTo(undefined,5);
+    expect(result1).to.equal(5);
+    });
+    it("Default for falsy case 2",function(){
+        const result2 = defaultTo(null,10);
+        expect(result2).to.equal(10);
+        });
+
+
+    });
