6124b90700c4fd6ca43b244562e22cf6a23816cd
Niladri Saha <niladri.saha@tuni.fi>
2020-12-01 17:13:44 +0530

    Testing

--- src/divide_test.js
+++ src/divide_test.js
@@ -1,11 +1,10 @@
-// import chai from 'chai';
-// const expect = chai.expect;
-// const assert = chai.assert;
+ import chai from 'chai';
+const expect = chai.expect;
+const assert = chai.assert;
 // //const {expect,assert} = require('chai');
-// import divide from '../divide.js';
+import divide from '../divide.js';
 // //const fs = require("../add");
     
-<<<<<<< HEAD:src/divide_test.js
 describe("Divide Integration Testing",()=>{
     it("Case 1 for integers operands > 0", function(){
     const result = divide(6,4)
@@ -15,24 +14,12 @@ describe("Divide Integration Testing",()=>{
     it("Case 2 for float operands > 0",function(){
         const result = divide(5.0,2.0);
         expect(result).to.equal(2.5);
-=======
-// describe("Divide Integration Testing",()=>{
-//     it("Case 1 for integers operands > 0", function(){
-//     const result = divide(5,2)
-//    // console.log(result);
-//     expect(result).to.equal(2.5);
-//     });
-//     it("Case 2 for float operands > 0",function(){
-//         const result = divide(5.0,2.0);
-//         expect(result).to.equal(2.5);
->>>>>>> 4b917fecc37a338457b9227799177c88e93d1e11:src/test/divide_test.js
         
-//     });
-//     it("Case 3 for mixed operands > 0",function(){
-//         const result = divide(6.0,3);
-//         expect(result).to.equal(2.0);
+    });
+     it("Case 3 for mixed operands > 0",function(){
+       const result = divide(6.0,3);
+       expect(result).to.equal(2.0);
         
-<<<<<<< HEAD:src/divide_test.js
     });
     it("Divide two non-numbers and throws error",function(){
         try {
@@ -41,18 +28,8 @@ describe("Divide Integration Testing",()=>{
        catch(e){
            console.log(e);
        }});
-=======
-//     });
-//     it("Divide two non-numbers and throws error",function(){
-//         try {
-//        const string_res = divide("Niladri","Saha");
-//        assert.fail("Error thrown!");}
-//        catch(e){
-//            expect(e).to.equal("Bad Parameters!");
-//        }});
->>>>>>> 4b917fecc37a338457b9227799177c88e93d1e11:src/test/divide_test.js
    
         
 
 
-//     });
+   });
